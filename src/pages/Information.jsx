1ae8a8204337a3b1b76d602546fe3a9bd43c5049
YongLeeCode <yonghwan311@gmail.com>
2024-02-17 14:06:10 -0700

    css

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../styles/base.css';
 
 export default function Information() {
 	return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import '../styles/base.css';
 import { login } from '../api/authClient';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
